3c258c0544c1e7d384b3637bb0ff9388cf5468cc
eetann <hideharu092@gmail.com>
2024-06-17 15:02:45 +0900

    ci: svelte.configをファイル化

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "node:path";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  modules: ['@wxt-dev/module-svelte'],
+	manifest: {
+		name: "wxt-content-daisyui",
+		version: "1.0.0",
+	},
+	srcDir: "src",
+	vite: () => ({
+		plugins: [
+			svelte({
+				configFile: path.resolve("./svelte.config.mjs"),
+			}),
+		],
+	}),
 });
